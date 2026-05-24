3059287edc03615103b726d5fbf67baf50f442c9
Mark <993600205@qq.com>
2026-05-24 21:40:36 +0800

    fix: add ListenTogether stub, delete remaining dead files

---
+++ src/renderer/src/store/ListenTogether.ts
@@ -0,0 +1,10 @@
+// Stub: listen together feature removed
+import { defineStore } from 'pinia'
+export const useListenTogetherStore = defineStore('listenTogether', {
+  state: () => ({
+    isInRoom: false,
+    current: { song: null as any }
+  }),
+  getters: {},
+  actions: {}
+})
