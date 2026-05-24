b415a7952b989c4dc143bf945210d76ab9e0d9a1
Mark <993600205@qq.com>
2026-05-24 21:21:52 +0800

    fix: add missing auth stub properties

--- src/renderer/src/store/Auth.ts
+++ src/renderer/src/store/Auth.ts
@@ -2,6 +2,10 @@
 import { defineStore } from 'pinia'
 export const useAuthStore = defineStore('auth', {
   state: () => ({}),
-  getters: {},
-  actions: {}
+  getters: {
+    isAuthenticated: () => false
+  },
+  actions: {
+    outlogin() {}
+  }
 })
